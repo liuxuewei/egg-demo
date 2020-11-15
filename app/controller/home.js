0c1cd19272f3d12db1c5f6b724bf457de061dc32
liuxuewei <xuewei.lxw@alibaba-inc.com>
2020-11-15 13:15:47 +0800

    添加参数

--- app/controller/home.js
+++ app/controller/home.js
@@ -5,7 +5,8 @@ const Controller = require('egg').Controller;
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
-    ctx.body = 'hi, egg';
+    const params = ctx.query;
+    ctx.body = `hi, ${params.name || 'egg'}`;
   }
 }
 
