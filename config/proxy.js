3983e3057fdd0eecc0ee9b01e0a58317c05ca4a6
liuxuewei <xuewei.lxw@alibaba-inc.com>
2021-11-23 15:01:02 +0800

    up

--- config/proxy.js
+++ config/proxy.js
@@ -4,36 +4,14 @@ const path = require('path');
 module.exports = {
     eggFramework: 'egg',
     directoryToJar: path.join(__dirname, '../libs'),
-    mavenRepository: 'http://mvnonline.alibaba-inc.com/mvn/repository',
+    // mavenRepository: 'http://mvnonline.alibaba-inc.com/mvn/repository',
     group: 'HSF',
     version: '1.0.0',
     uniqueId: 'uniqueId',
     tpl: 'proxy.js.tpl',
     method: {},
     responseTimeout: 10000,
-    services: [
-        {
-            appname: 'onetouch-basedata',
-            api: {
-                IPartnerUserRemoteService: {
-                    interfaceName: 'com.alibaba.onetouch.basedata.user.client.IPartnerUserRemoteService',
-                },
-                iPartnerCustomerRemoteServiceForInterceptor: {
-                    interfaceName: 'com.alibaba.onetouch.basedata.partnerCustomer.IPartnerCustomerRemoteService',
-                },
-            },
-            dependency: [
-                {
-                    groupId: 'com.alibaba.onetouch',
-                    artifactId: 'gaea-client',
-                    version: '1.0.37',
-                },
-                {
-                    groupId: 'com.alibaba.onetouch',
-                    artifactId: 'onetouch-base-api',
-                    version: '2.3.7',
-                },
-            ],
-        }
-    ],
+    services: require('fs').readdirSync(`${__dirname}/proxy-cfg/`)
+    .filter(name => name.match(/.js$/))
+    .map(name => require(`${__dirname}/proxy-cfg/${name}`)),
 };
